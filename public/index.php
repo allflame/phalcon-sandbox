851170455ef37b6258ba4b38e8dec5e496fe093e
allflame <le@mirohost.net>
2016-05-23 12:20:20 +0300

    Reference realization

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require 'bootstrap.php';
 try {
     $application = $di->get('app.self');
     $di->get('app.bootstrapper.factory')->createBootstrapper()->bootstrap($application);
-    $application->handle();//->send();
+    $application->handle()->send();
 } catch (\Exception $e) {
      echo "Exception: ", $e->getMessage();
 }
