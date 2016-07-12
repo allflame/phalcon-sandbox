019549c7bfa72ebe57ee1cfb6e642ac7161592ca
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-07-12 13:16:28 +0300

    Added PHPDoc

--- src/Command/TestTimeCommand.php
+++ src/Command/TestTimeCommand.php
@@ -36,18 +36,20 @@ class TestTimeCommand
      */
     public function execute()
     {
-        $london = new \DateTimeZone('Europe/London');
+        $london = new \DateTimeZone('America/Phoenix');
         $utc = new \DateTimeZone('UTC');
         $time1 = new \DateTime('now', $london);
         $time2 = clone $time1;
         echo $time1->format('Y-m-d H:i:s T') . "\n";
         echo $time2->setTimezone($utc)->format('Y-m-d H:i:s T') . "\n";
-        $time3 = $this->timeFactory->createFromString('now', 'Europe/London');
+        $time3 = $this->timeFactory->createFromString('now', 'America/Denver');
         $time4 = $this->timeFactory->createFromString('now', 'UTC');
+        echo $time3->format(DATE_W3C);
         echo $time3->format('Y-m-d H:i:s T')  . "\n";
         echo $time4->format('Y-m-d H:i:s T')  . "\n";
         echo $time3->toSystem()  . "\n";
         echo $time4->toSystem()  . "\n";
+        echo $time3->getTimezone()->getSynonym() . "\n";
         $time5 = $this->timeFactory->createFromString('now', 'Europe/London (BST GMT+0100)');
         echo $time5->toSystem();
     }
