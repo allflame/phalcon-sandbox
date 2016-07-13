07ca4bf5bd6bc5a478479b4f09eb57c243c68dfe
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-07-13 10:21:52 +0300

    Added example

--- src/Command/TestTimeCommand.php
+++ src/Command/TestTimeCommand.php
@@ -11,6 +11,7 @@
 namespace App\Command;
 
 use Vain\Time\Factory\TimeFactoryInterface;
+use Vain\Time\Zone\TimeZone;
 
 /**
  * Class TestTimeCommand
@@ -36,22 +37,9 @@ class TestTimeCommand
      */
     public function execute()
     {
-        $london = new \DateTimeZone('America/Phoenix');
-        $utc = new \DateTimeZone('UTC');
-        $time1 = new \DateTime('now', $london);
-        $time2 = clone $time1;
-        echo $time1->format('Y-m-d H:i:s T') . "\n";
-        echo $time2->setTimezone($utc)->format('Y-m-d H:i:s T') . "\n";
         $time3 = $this->timeFactory->createFromString('now', 'America/Denver');
-        $time4 = $this->timeFactory->createFromString('now', 'UTC');
-        echo $time3->format(DATE_W3C);
-        echo $time3->format('Y-m-d H:i:s T')  . "\n";
-        echo $time4->format('Y-m-d H:i:s T')  . "\n";
-        echo $time3->getTimeZoneSpec() . "\n";
-        echo $time3->toSystem()  . "\n";
-        echo $time4->toSystem()  . "\n";
-        echo $time3->getTimezone()->getSynonym() . "\n";
-        $time5 = $this->timeFactory->createFromString('now', 'Europe/London (BST GMT+0100)');
-        echo $time5->toSystem();
+        $time4 = $time3->setTimezone(new TimeZone('Europe/Kiev', 'Europe/Kiev', 'EET'));
+        echo $time3->format(DATE_W3C) . "\n";
+        echo $time4->format(DATE_W3C);
     }
 }
